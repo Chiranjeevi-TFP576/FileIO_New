afd1cab8450c4676965db885b3fad5b4a922e2e7
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-02-03 22:33:31 +0530

    [chiru] Added AddressBook UC_13 Using FileIo

---
+++ ReadAndWriteFile_IO/FileInputs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadAndWriteFile_IO
+{
+    public class FileInputs
+    {
+        public string path = @"D:\Batch 244\ReadAndWriteFile_IO\PersonDetails.txt";
+        public void ReadFile()
+        {
+            if (File.Exists(path))
+            {
+                using (StreamReader streamReader = File.OpenText(path))
+                {
+                    string data = String.Empty;
+                    while ((data = streamReader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(data);
+                    }
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nFile Not Found !");
+            }
+        }
+        public void WriteFile()
+        {
+            string path = @"D:\Batch 244\ReadAndWriteFile_IO\PersonDetails.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                string data = "\nAddressBook";
+                sw.WriteLine(data);
+                sw.Close();
+                Console.WriteLine(File.ReadAllText(path));
+                Console.ReadLine();
+            }
+            
+        }
+    }
+
+
+
+}       
+        
+    
+
---
+++ ReadAndWriteFile_IO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace ReadAndWriteFile_IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome File IO Operations");
+            FileInputs fileInputs = new FileInputs();
+            fileInputs.ReadFile();
+            fileInputs.WriteFile();
+        }
+    }
+}
